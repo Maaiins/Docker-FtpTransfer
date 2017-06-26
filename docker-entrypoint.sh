05a639e6e950856fd26027dfc56c2b67209ef683
Nicolai Lauser <n.lauser@polytec.de>
2017-06-26 08:47:08 +0200

    try to get exit status

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -203,7 +203,7 @@ cat >&1 <<-EOT
 EOT
 
 ncftpget -R -T -v -u ${FTP_SOURCE_USER} -p ${FTP_SOURCE_PASSWORD} -P ${FTP_SOURCE_PORT} ${FTP_SOURCE_ADDRESS} '/ftp' "${FTP_SOURCE_DIR}" 2>&1
-
+echo $?
 # ----
 # Exclude uploaded folder
 if [ -z ${FTP_TARGET_EXCLUDE_DIR} ]; then
