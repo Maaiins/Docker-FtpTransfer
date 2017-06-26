2e2c85d9797f168b1d5f83c7b139e76d02365fa5
Nicolai Lauser <n.lauser@polytec.de>
2017-06-26 12:12:28 +0200

    Why the hell stops the script the execution?

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -142,8 +142,8 @@ transfer ()
 {
 # ----
 # Cleanup
-#rm -rf '/ftp'
-#mkdir -p '/ftp'
+rm -rf '/ftp'
+mkdir -p '/ftp'
 
 cat >&1 <<-EOT
 
@@ -202,8 +202,7 @@ cat >&1 <<-EOT
 
 EOT
 
-ncftpget -R -T -v -u ${FTP_SOURCE_USER} -p ${FTP_SOURCE_PASSWORD} -P ${FTP_SOURCE_PORT} ${FTP_SOURCE_ADDRESS} '/ftp' "${FTP_SOURCE_DIR}" 2>&1
-echo $?
+ncftpget -R -T -v -u ${FTP_SOURCE_USER} -p ${FTP_SOURCE_PASSWORD} -P ${FTP_SOURCE_PORT} ${FTP_SOURCE_ADDRESS} '/ftp' "${FTP_SOURCE_DIR}" && echo success || echo epic fail
 # ----
 # Exclude uploaded folder
 if [ -z ${FTP_TARGET_EXCLUDE_DIR} ]; then
